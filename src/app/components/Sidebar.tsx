5dad79ada7731ff280c0e2543df35e7a4583d7a9
Hosoo Cha <hosoo.cha@gmail.com>
2024-10-20 22:21:45 +1300

    sidebar - Link + active link styling

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,10 +1,14 @@
+'use client'
+
 import React, { ReactNode } from 'react'
 import { RxDashboard } from "react-icons/rx";
 import { BiFoodMenu } from "react-icons/bi";
-import { MdOutlinePalette, MdOutlinePermMedia } from "react-icons/md";
+import { MdOutlinePalette, MdOutlinePermMedia, MdQrCode } from "react-icons/md";
 import { GoHistory } from "react-icons/go";
-import { MdQrCode } from "react-icons/md";
-import { IconType } from 'react-icons';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+
 
 interface SidebarLink{
     name: string
@@ -13,16 +17,19 @@ interface SidebarLink{
 }
 
 const Sidebar = () => {
-    const sidebararr : SidebarLink[] = [{name: "Dashboard", icon: <RxDashboard />, link: "dashboard"}, {name: "Menu", icon: <BiFoodMenu />, link: "manage-menu"}]
+    const currentPath = usePathname();
+    const sidebararr : SidebarLink[] = [{name: "Dashboard", icon: <RxDashboard />, link: "/dashboard"}, {name: "Menu", icon: <BiFoodMenu />, link: "/manage-menu"}, {name: "Appearance", icon: <MdOutlinePalette />, link: "/manage-appearance"}, {name: "Media", icon: <MdOutlinePermMedia />, link: "/manage-media"}, {name: "Activity Log", icon: <GoHistory />, link: "/activity-log"}, {name: "QR Links", icon: <MdQrCode />, link: "/manage-qr-links"}]
     
   return (
-    <div className='font-inter -full bg-my-primary-950'>
-        {sidebararr.map((item: SidebarLink, i:number) => (
-            <button className='flex text-my-black-600'>
-                {item.icon}
-                <p>{item.name}</p>
-            </button>
-        ))}
+    <div className='font-inter -full bg-my-primary-950 px-4 py-3'>
+        <div className='flex flex-col gap-1'>
+            {sidebararr.map((item: SidebarLink, i:number) => (
+                <Link href={`${item.link}`} key={i} className={`${currentPath === item.link ? "bg-primary-comp-800 text-white" : "text-my-black-600"}  flex gap-3 items-center w-full py-3 px-3 rounded-md `}>
+                    <div className='flex text-[1.3rem]'>{item.icon}</div>
+                    <p className='text-[0.9rem]  tracking-wide'>{item.name}</p>
+                </Link>
+            ))}
+        </div>
     </div>
   )
 }
