9ed8f967182e9415c38b6b3e6daaed1ec889e35a
Hosoo Cha <hosoo.cha@gmail.com>
2024-11-01 16:55:24 +1300

    header - icon + view my menu styles

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,8 +1,21 @@
+'use client'
 import React from 'react'
+import { PiEye } from "react-icons/pi";
 
 const Header = () => {
   return (
-    <div className='w-full h-[3.5rem] bg-my-primary-950'>Header</div>
+    <div className='w-full h-[3.5rem] grid grid-cols-[17rem_auto] bg-my-primary-950 py-2'>
+      <div className='h-full flex items-center justify-between px-5'>
+        <img src={"/smartmenuicon.svg"} className='h-[2rem] w-auto'/>
+        <button className='h-[2rem] flex text-[#3FA9F5] bg-my-primary-900 rounded-[0.2rem] px-3 gap-2 items-center '>
+          <PiEye className='flex text-[1.2rem]'/>
+          <p className='text-sm'>My Menu</p>
+        </button>
+      </div>
+      <div>
+
+      </div>
+    </div>
   )
 }
 
