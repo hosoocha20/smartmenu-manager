f3881b26a477783957c52c39d627f0a6f5f7a7f2
Hosoo Cha <hosoo.cha@gmail.com>
2024-11-01 20:37:30 +1300

    /dashboard - profile styles

--- src/app/(auth)/dashboard/page.tsx
+++ src/app/(auth)/dashboard/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { ReactNode } from "react";
+import React, { Fragment, ReactNode } from "react";
 import { FiPlusSquare } from "react-icons/fi";
 import {
   MdOutlineTableBar,
@@ -8,8 +8,10 @@ import {
   MdOutlineCategory,
 } from "react-icons/md";
 import { PiEye } from "react-icons/pi";
-import { IoEarth } from "react-icons/io5";
+import { IoEarth,  } from "react-icons/io5";
+
 import { TbBulb } from "react-icons/tb";
+import { LuSettings } from "react-icons/lu";
 
 interface GetStartedLinksI {
   name: string;
@@ -61,12 +63,12 @@ const Dashboard = () => {
             </h4>
             <div className="mt-2 grid grid-cols-[min-content_auto] gap-y-2 gap-x-3 text-my-primary-900 items-center">
               {getStartedArr.map((el: GetStartedLinksI, i: number) => (
-                <>
+                <Fragment key={i}>
                   <div className="flex text-[1.1rem]">{el.icon}</div>
                   <p className="text-my-accent-400 text-sm font-medium tracking-wide">
                     {el.name}
                   </p>
-                </>
+                </Fragment>
               ))}
             </div>
           </div>
@@ -84,14 +86,14 @@ const Dashboard = () => {
           <h2 className="text-[1.1rem] text-my-primary-900 font-medium tracking-wide">
             Overview
           </h2>
-          <div className="py-4  flex  w-[90%] mx-auto my-0 ">
+          <div className=" py-8  flex  w-[90%] mx-auto my-0 ">
             {overviewArr.map((el: OverviewI, i: number) => (
               <div key={i} className="text-my-primary-900 flex gap-2 flex-1">
                 <div className={`text-white ${i === 0 ? "bg-my-secondary-300" : i === 1 ? "bg-royal-blue-500" : "bg-my-dark-700"}  text-[1.7rem] flex items-center justify-center  w-[3.2rem] h-[3.2rem] rounded-full`}>
                   {el.icon}
                 </div>
                 <div className="mt-[-0.5rem]">
-                  <p className="text-sm">{el.name}</p>
+                  <p className="text-sm text-[#636A7A]">{el.name}</p>
                   <p className="text-[1.9rem]  leading-none font-bold">7</p>
                 </div>
               </div>
@@ -99,10 +101,18 @@ const Dashboard = () => {
           </div>
         </div>
         {/*Profile */}
-        <div className="dashboard-profile w-full border border-[#CCCCCC] border-opacity-50 bg-white px-5 py-3 rounded-md">
-          <h2 className="text-[1.1rem] text-my-primary-900 font-medium tracking-wide">
-            Overview
-          </h2>
+        <div className="dashboard-profile w-full  gap-4 border border-[#CCCCCC] border-opacity-50 bg-white px-5 py-3 rounded-md">
+          <div className="flex items-center justify-between">
+            <h2 className="text-[1.1rem] text-my-primary-900 font-medium tracking-wide">
+              Your Profile
+            </h2>
+            <LuSettings className="flex text-[#808080] text-[1.2rem]"/>
+          </div>
+          <div className="mt-4 flex flex-col gap-1 text-[0.9rem] text-my-primary-900">
+            <p ><span className="font-medium">Restaurant Name:</span> SmartMenu</p>
+            <p ><span className="font-medium">Admin Name:</span> John Doe</p>
+            <p ><span className="font-medium">Last Login:</span> 8:26pm 1/11/2024</p>
+          </div>
         </div>
         {/*Activity */}
         <div className="dashboard-activity  w-full  border border-[#CCCCCC] border-opacity-50 bg-white px-5 py-3 rounded-md">
