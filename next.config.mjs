1f0e9d4fea68df2b074f24029fe6fefb9e5f5b92
Hosoo Cha <hosoo.cha@gmail.com>
2024-11-02 20:44:23 +1300

    next.congif - redirect manage-menu to default menuCategory

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+          {
+            source: '/manage-menu',
+            destination: '/manage-menu/menuCategory',
+            permanent: true, // Permanent redirect
+          },
+        ];
+      },
+};
+
+
 
 export default nextConfig;
